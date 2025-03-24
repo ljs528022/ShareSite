b5f45e715c0cbb477b6eed959de9773f7b43fd5f
ljs528022 <ljs528022@gmail.com>
2025-03-24 14:26:11 +0900

    사이트 제작 Main Page - 1.4
    itemCard 작성 중...
    
    // TODO
    paging 에 필요한 버튼들을 상품 갯수에 따라 출력해야함.
    Small Card도 완성해야 함.
    item을 더 만들 수 있으면 좋겠음...2

--- src/main/frontend/src/components/itemCard.jsx
+++ src/main/frontend/src/components/itemCard.jsx
@@ -1,12 +1,10 @@
-
-import { Badge } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import style from "../components/css/itemCard.module.css"
 
 const SmallItemCard = ({ items }) => {
 
     return (
         <>
-            <div className="SmallCard">
+            <div className="Small_Card_wrapper">
                 {/* <Card>
                     <Card.Img variant="top" src=""/>
                     <Card.Body>
@@ -36,29 +34,51 @@ const NormalItemCard = ({ items }) => {
     return (
         <>
             {item.length > 0 && item.map((i) => (
-                <div className='Card_wrapper' key={i.itemKey}>
-                    <img className='CardImg' src={`./public/SSLogo.jpg`} />
-                    <div className='CardBody'>
-                        <p>{i.subject}</p>
-                        <p>{i.price}</p>
-                        <Badge bg={i.tradestatus == 0 ? "danger" : "success"}>{i.tradestatus == 0 ? "거래중" : "거래완료"}</Badge>
-                        
+                <div className={style.Normal_Card_wrapper} key={i.itemKey}>
+                    <img className={style.Normal_Card_img} src={`./public/SSLogo.jpg`} />
+                    <div className={style.Normal_CardBody}>
+                        <p className={style.subject}>{i.subject}</p>
+                        <p className={style.price}>{i.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')} 원</p>
+                        <span className={i.tradestatus == 0 ? style.span_trading : style.span_traded}
+                        >{i.tradestatus == 0 ? "거래중" : "거래완료"}</span>
+
                         {/* 등록된 시간을 기준으로 지금 시간으로 부터 얼마나 되었는지 표시 */}
-                        <p>몇분 전</p>
+                        <span className={style.span_time}>{getDayMinuteCounter(i.writeDate)}</span>
                     </div>
                 </div>
-                    // <Card key={i.itemKey}> 
-                    //     <Card.Img variant="top" src="./public/SSLogo.jpg"/>
-                    //     <Card.Body>
-                    //         <Card.Title>{i.subject}</Card.Title>
-                    //         <p>{i.price}</p>
-                    //         <Badge bg={`${i.tradestatus} == 0 ? 'danger' : 'success'`}>{i.tradestatus == 0 ? "거래중" : "거래완료"}</Badge>
-                    //         <p>몇분전</p>
-                    //     </Card.Body>
-                    // </Card>
             ))}
         </>
     );
+}
+
+const getDayMinuteCounter = (date) => {
+    if (!date) {return "";}
+
+    let today = new Date();
+    let postdate = new Date(date);
+    let elapsedTime = Math.trunc((today - postdate) / 1000);
+    let elapsedText = "";
+
+    const seconds = 1;
+    const minute = seconds * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    if(elapsedTime < seconds) {
+        elapsedText = "방금전";
+    } else if (elapsedTime < minute) {
+        elapsedText = elapsedTime + "초 전";
+    } else if (elapsedTime < hour) {
+        elapsedText = Math.trunc(elapsedTime / minute) + "분 전";
+    } else if (elapsedTime < day) {
+        elapsedText = Math.trunc(elapsedTime / hour) + "시간간 전";
+    } else if (elapsedTime < (day * 15)) {
+        elapsedText = Math.trunc(elapsedTime / day) + "분 전";
+    } else {
+        elapsedText = postdate.toLocaleDateString("ko-KR", {dateStyle: "medium",})
+    }
+
+    return elapsedText;
 }
 
 export { SmallItemCard, NormalItemCard };
