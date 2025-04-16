c079bfe3e97d80b195698adca77c727e45c5dfde
ljs528022 <ljs528022@gmail.com>
2025-04-16 14:06:36 +0900

    사이트 제작 Item Page 1
    // TODO
    상품 제작 페이지 시작!
    >> 디자인은 카테고리만 완료하면 됨!
    >> 이미지를 첨부하면 서버에 저장하고, 첫번째 이미지를 대표 이미지로 지정 >> 확인 필요
    >> 구글 지도 API 써서 장소 저장 기능 만들기
    >> 장소는 여러 곳이니 List로 받아야 할 듯? (테이블을 따로 만들어야 할 수 있음)
    
    (확인해야 할 것) 인증 토큰 만료 후에 refresh나 자동 로그인이 되는가?
    >> 그냥 로그아웃 or 토큰 갱신. 둘 중 하나 선택
    
    // 네이버, 카카오 간편로그인 구현해야함!

--- src/main/frontend/src/pages/item/itemWrite.jsx
+++ src/main/frontend/src/pages/item/itemWrite.jsx
@@ -21,9 +21,11 @@ const ItemWrite = () => {
         price: 0,
         loacation: "",
         itemtype: "",
-        purtype: "",
+        purtype: 0,
         img: [],
     });
+
+    const [ formattedPrice, setFormattedPrice ] = useState('');
 
     useEffect(() => {
         const fetchCategories = async () => {
@@ -66,21 +68,17 @@ const ItemWrite = () => {
         
     }
 
+    const handlePriceChange = (e) => {
+        const rawValue = e.target.value.replace(/[^0-9]/g, "");
+        const formatted = Number(rawValue).toLocaleString("ko-KR");
+        setItemData(prev => ({ ...prev, price: rawValue}));
+        setFormattedPrice(formatted);
+    }
+
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        const formData = new FormData();
-        formData.append("item", new Blob([JSON.stringify(itemData)], { type: "application/json" }));
-
-        for(let i = 0; i < itemData.img.length; i++) {
-            formData.append("img", itemData.img[i]);
-        }
-
-        await postData("/product/item", formData, {
-            headers: {
-                "Content-Type": "multipart/form-data",
-            },
-        });
+        
     }
 
 
@@ -92,15 +90,22 @@ const ItemWrite = () => {
                     {/* 이미지 첨부 */}
                     <div className="form-row">
                         <div className="input-img">
-                            <input id="img" type="file" accept="image/*" multiple onChange={handleImageChange} />
-                            <span>{`${itemData.img.length} / 5`}</span>
+                            <label htmlFor="img">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 512 512">
+                                    <path fill="#575757" d="M220.6 121.2L271.1 96 448 96l0 96-114.8 0c-21.9-15.1-48.5-24-77.2-24s-55.2 8.9-77.2 24L64 192l0-64 128 0c9.9 0 19.7-2.3 28.6-6.8zM0 128L0 416c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-320c0-35.3-28.7-64-64-64L271.1 32c-9.9 0-19.7 2.3-28.6 6.8L192 64l-32 0 0-16c0-8.8-7.2-16-16-16L80 32c-8.8 0-16 7.2-16 16l0 16C28.7 64 0 92.7 0 128zM168 304a88 88 0 1 1 176 0 88 88 0 1 1 -176 0z"/>
+                                </svg>
+                                <span>{`${itemData.img.length} / 5`}</span>
+                            </label>
+                            <input id="img" type="file" accept="image/*" multiple onChange={handleImageChange} style={{ display: "none"}} />
+                            <div>
+
+                            </div>
                         </div>
                     </div> 
                     {/* 상품명 */}
                     <div className="form-row">
                         <div className="input-subject">
-                            
-                            <input id="subject" type="text" onChange={handleInput} />
+                            <input id="subject" type="text" onChange={handleInput} placeholder="상품명"/>
                         </div>
                     </div>
                     {/* 카테고리 선택란 */}
@@ -113,7 +118,10 @@ const ItemWrite = () => {
                     {/* 가격란 */}
                     <div className="form-row">
                         <div className="input-price">
-                            <input id="price" type="text" onChange={handleInput} />
+                            <div className="input-price-wrapper">
+                                <span>₩</span>
+                                <input id="price" type="text" value={formattedPrice} onChange={handlePriceChange}/>
+                            </div>
                         </div>
                     </div>
                     {/* 상품 설명 란 */}
@@ -125,21 +133,53 @@ const ItemWrite = () => {
                     {/* 상품 상태 선택란 */}
                     <div className="form-row">
                         <div className="input-itemtype">
-                            <input id="itemtype" onChange={handleInput} />
+                            <label>상품 상태</label>
+                            <div className="radio-group">
+                                <label className="radio-box">
+                                    <input id="itemtype" type="radio" value={"NEW"} onChange={handleInput} checked={itemData.itemtype === "NEW"}/>
+                                    <span>신품</span>
+                                </label>
+                                <label className="radio-box">
+                                    <input id="itemtype" type="radio" value={"OLD"} onChange={handleInput} checked={itemData.itemtype === "OLD"}/>
+                                    <span>중고</span>
+                                </label>
+                            </div>
                         </div>
                     </div>
 
                     {/* 거래 방식 */}
                     <div className="form-row">
                         <div className="input-purtype">
-                            <input id="purtype" onChange={handleInput} />
+                        <label>상품 상태</label>
+                            <div className="radio-group">
+                                <label className="radio-box">
+                                    <input id="purtype" type="radio" value={2} onChange={handleInput} checked={itemData.purtype == 2}/>
+                                    <span>직거래</span>
+                                </label>
+                                <label className="radio-box">
+                                    <input id="purtype" type="radio" value={1} onChange={handleInput} checked={itemData.purtype == 1}/>
+                                    <span>택배거래</span>
+                                </label>
+                                <label className="radio-box">
+                                    <input id="purtype" type="radio" value={0} onChange={handleInput} checked={itemData.purtype == 0}/>
+                                    <span>둘다가능</span>
+                                </label>
+                            </div>
                         </div>
                     </div>
 
                     {/* 희망지역 */}
                     <div className="form-row">
                         <div className="location-btn">
-                            
+                            <label>희망지역</label>
+                            <button type="button" onClick={null}>
+                                + 추가하기
+                            </button>
+                            <div className="location-box">
+                                <ul>
+                                    
+                                </ul>
+                            </div>
                         </div>
                     </div>  
                     {/* 등록버튼 */}
