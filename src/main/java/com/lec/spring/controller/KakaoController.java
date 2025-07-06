11987f17cdd2e9fb58919dbc4a8910a314d0ef2a
ljs528022 <ljs528022@gmail.com>
2025-07-06 11:23:34 +0900

    사이트 제작 간편로그인 API 0.2.5
    // TODO
    >> 카카오 간편 로그인 추가 중...
    >> 탈퇴 완료 페이지에서 네이버 서비스 끊기 방법 유도 해야함.
    --------------------------------
    [만들어야 할 것]
    >> 관리자 페이지
    
    // Edit
    >> 카카오 간편 로그인 준비

--- src/main/java/com/lec/spring/controller/KakaoController.java
+++ src/main/java/com/lec/spring/controller/KakaoController.java
@@ -1,4 +1,16 @@
 package com.lec.spring.controller;
 
+import com.lec.spring.service.KakaoLoginService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/oauth/kakao")
+@RequiredArgsConstructor
 public class KakaoController {
+
+    @Autowired
+    private KakaoLoginService kakaoLoginService;
 }
